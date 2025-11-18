691982ae8d8bad47248444c86e4a1fd03554b379
emily d'achiardi <sneakyfelixed@gmail.com>
2025-11-18 14:23:41 -0800

    initial commit

---
+++ bookmarklet.js
@@ -0,0 +1,7 @@
+(function () {
+    console.log("bookmarklet starting");
+    let paragraphs = document.getElementsByTagName('p');
+    for (let i = 0; i < paragraphs.length; i++) {
+        paragraphs[i].innerHTML = 'kitten'; 
+    }
+}) (); 
