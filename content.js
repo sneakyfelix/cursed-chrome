39061408293a459653eb1bea922b08d6d15aa05a
emily d'achiardi <sneakyfelixed@gmail.com>
2025-11-18 23:48:42 -0800

    updated
    updated text styling and animation for elements.

--- content.js
+++ content.js
@@ -1,18 +1,34 @@
 console.log("chrome extension is ready to go"); 
 
-let words = document.getElementsByTagName('p');
+// let paragraph = document.getElementsByTagName('p');
+let words = document.querySelectorAll(
+    'p, h1, h2, h3, h4, h5, h6, span, a, li, blockquote, figcaption, div, section, article, button, label, strong, em, code'
+  );
+  
 
 
-for (elt of words) {
-    elt.style['fontSize'] = '12em';
-    elt.style['color'] = "blue";
+for ( let elt of words) {
 
-    let text = elt.textContent;
-    let newText = text.replace(/[A-Za-z]/g, 'e');
-    elt.textContent = newText;
+    elt.style['color'] = "orange";
+    elt.style['text-shadow'] = "1px 1px 2px red, 0 0 25px orange, 0 0 5px yellow";
+    elt.style['display'] = "inline-block";
+
+    setInterval(function() {
+        let randomx = Math.floor(Math.random() * 5) - 2;
+        let randomy = Math.floor(Math.random() * 10) - 3;
+        elt.style.transform = `translate(${randomx}px, ${randomy}px)`;
+    }, 80);
+
+    setInterval(function() {
+        let randomfont = Math.floor(Math.random() * 300) - 2;
+        elt.style['fontSize'] = `${randomfont}px`;
+    }, 2080);
+
 }
 
 
+
 let body = document.body; 
-body.style.backgroundColor = '#FFFFFF';
+body.style.backgroundColor = '#000000';
+
 
